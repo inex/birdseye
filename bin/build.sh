fac40ddd0f6fc13ed6f733b69be0ad1544511ceb
Barry O'Donovan <barry@opensolutions.ie>
2016-10-23 16:06:43 +0200

    Sample cmd line to package

---
+++ bin/build.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+# sample package command line
+
+# tar --exclude=birdseye-1.0.0/.env
+    # --exclude='birdseye-1.0.0/birdseye-*.env'
+    # --exclude='birdseye-1.0.0/.git'
+    # --exclude='birdseye-1.0.0/.vagrant'
+    # --exclude='*~'
+    # --exclude='*log'
+    # -vcjf
+    # birdseye-1.0.0.tar.bz2 birdseye-1.0.0
