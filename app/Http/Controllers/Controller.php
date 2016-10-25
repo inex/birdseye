883f038ca4c4cb2f69993249dace67d0edc835b1
Barry O'Donovan <barry@opensolutions.ie>
2016-10-25 10:42:08 +0100

    Harden v6 validation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -53,12 +53,48 @@ class Controller extends BaseController
 
     protected function assertValidPrefix($net) {
         // validate net as a IP / network
-        if( !( preg_match( "/^([0-9]{1,3}\.){3}[0-9]{1,3}(\/\d{1,2}){0,1}$/", $net)
-                || preg_match( "/^[a-f0-9\:]+(\/\d{1,3}){0,1}$/", $net ) ) ) {
+        if( !strpos($net,':') ) {
+            if( preg_match( "/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}(:?\/\d{1,2}){0,1}$/", $net ) ) {
+                return true;
+            }
+        } else {
+            // from Zend/Validate/Ip.php (BSD)
 
-            // FIXME: a better v6 checker would be useful
-            abort(400,'Bad IP address');
+            // pull the mask if it's set
+            if( strpos($net, '/') ) {
+                $mask = substr( $net, strpos($net, '/')+1 );
+                if( !( preg_match( "/^[0-9]{1,3}$/", $mask ) && $mask >= 0 && $mask <= 128 ) ) {
+                    abort(400,'Bad IP address');
+                }
+
+                $net = substr($net, 0, strpos($net, '/') );
+            }
+
+            if( strpos($net, '.') ) {
+                $lastcolon = strrpos($net, ':');
+                if (!($lastcolon && preg_match( "/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}(:?\/\d{1,2}){0,1}$/", substr($net, $lastcolon + 1 ) ) ) ) {
+                    abort(400,'Bad IP address');
+                }
+
+                $net = substr($net, 0, $lastcolon) . ':0:0';
+            }
+
+            if (strpos($net, '::') === false && preg_match('/\A(?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4}\z/i', $net) ) {
+                return true;
+            }
+
+            $colonCount = substr_count($net, ':');
+            if ($colonCount < 8 && preg_match('/\A(?::|(?:[a-f0-9]{1,4}:)+):(?:(?:[a-f0-9]{1,4}:)*[a-f0-9]{1,4})?\z/i', $net) ) {
+                return true;
+            }
+
+            // special case with ending or starting double colon
+            if( $colonCount == 8 && preg_match('/\A(?:::)?(?:[a-f0-9]{1,4}:){6}[a-f0-9]{1,4}(?:::)?\z/i', $net) ) {
+                return true;
+            }
         }
+
+        abort(400,'Bad IP address');
     }
 
 }
