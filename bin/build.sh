11a86951f0008f4c3b8163342655db7b22bd3f64
Barry O'Donovan <barry@opensolutions.ie>
2016-10-27 10:13:55 +0100

    Update release build hints

--- bin/build.sh
+++ bin/build.sh
@@ -2,11 +2,19 @@
 
 # sample package command line
 
-# tar --exclude=birdseye-1.0.0/.env
-    # --exclude='birdseye-1.0.0/birdseye-*.env'
-    # --exclude='birdseye-1.0.0/.git'
-    # --exclude='birdseye-1.0.0/.vagrant'
-    # --exclude='*~'
-    # --exclude='*log'
-    # -vcjf
-    # birdseye-1.0.0.tar.bz2 birdseye-1.0.0
+# param $1 should be the tag - e.g. v1.0.1 - which is assumed to be the current repo state
+
+echo mv birdseye birdseye-$1
+
+cat <<ENDTAR
+tar --exclude=birdseye-$1/.env               \\
+    --exclude='birdseye-$1/birdseye-*.env'   \\
+    --exclude='birdseye-$1/.git'             \\
+    --exclude='birdseye-$1/.vagrant'         \\
+    --exclude='*~'                           \\
+    --exclude='*log'                         \\
+    -vcjf                                    \\
+    birdseye-$1.tar.bz2 birdseye-$1
+ENDTAR
+
+echo mv birdseye-$1 birdseye
