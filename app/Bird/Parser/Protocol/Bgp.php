75e1fec0fea52fcafe71a725a1d982595e417b5d
Barry O'Donovan <barry@opensolutions.ie>
2019-02-07 13:34:58 +0000

    Bird2 compatible parsing of keepalive/hold timer

--- app/Bird/Parser/Protocol/Bgp.php
+++ app/Bird/Parser/Protocol/Bgp.php
@@ -139,12 +139,12 @@ class Bgp extends ProtocolParser
                 //     Route limit:      35/1000
                 $p['route_limit_at'] = intval( $matches[1] );
             }
-            else if( preg_match( "/^\s+Hold timer:\s+(\d+)\/(\d+)\s*$/", $line, $matches ) ) {
-                //     Hold timer:       124/180
+            else if( preg_match( "/^\s+Hold timer:\s+([\d\.]+)\/([\d\.]+)\s*$/", $line, $matches ) ) {
+                //     Hold timer:       124.5/180
                 $p['hold_timer'] = intval( $matches[2] );
             }
-            else if( preg_match( "/^\s+Keepalive timer:\s+(\d+)\/(\d+)\s*$/", $line, $matches ) ) {
-                //     Keepalive timer:  39/60
+            else if( preg_match( "/^\s+Keepalive timer:\s+([\d\.]+)\/([\d\.]+)\s*$/", $line, $matches ) ) {
+                //     Keepalive timer:  39.5/60
                 $p['keepalive'] = intval( $matches[2] );
             }
 
